c80e5ca28136a5dfccc15bd3a62f2783b87529d3
Skvorcov Evgeniy <creeger777@gmail.com>
2020-01-31 00:35:01 +0300

    adding ui

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using UnityEngine.UI;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Sprite _shirtСard;
+    [SerializeField] private Sprite _faceCard;
+
+    private Image _image;
+    private TextMeshProUGUI _numberCard;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void OnCardClick()
+    {
+        _image.sprite = _faceCard;
+    }
+}
+
