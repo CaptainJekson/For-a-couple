a9a35f3ea658a4599156c6c39c9d7ec8df1d75dc
Skvorcov Evgeniy <creeger777@gmail.com>
2020-01-31 03:33:25 +0300

    card initialization

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -9,18 +9,50 @@ public class Card : MonoBehaviour
 {
     [SerializeField] private Sprite _shirtСard;
     [SerializeField] private Sprite _faceCard;
+    [SerializeField] private float _timeShowing;
 
     private Image _image;
     private TextMeshProUGUI _numberCard;
+    private float _showingCooldown;
+
+    public bool IsGuessed { private get; set; }
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _numberCard = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (_showingCooldown > 0)
+            _showingCooldown -= Time.deltaTime;
+        else if(IsGuessed == false)
+            HideCard();
     }
 
     public void OnCardClick()
     {
+        ShowCard();
+    }
+
+    private void ShowCard()
+    {
         _image.sprite = _faceCard;
+        _numberCard.enabled = true;
+
+        _showingCooldown = _timeShowing;
+    }
+
+    private void HideCard()
+    {
+        _image.sprite = _shirtСard;
+        _numberCard.enabled = false;
+    }
+
+    public void SetNumberCard(int number)
+    {
+        _numberCard.text = number.ToString();
     }
 }
 
---
+++ Assets/Scripts/CardList.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardList : MonoBehaviour
+{
+    [SerializeField] Card _card;
+    [SerializeField] int _startingCountCards;
+
+    private List<Card> _cards;
+
+    private void Awake()
+    {
+        if (EvenCheckNumvers(_startingCountCards) == false)
+            _startingCountCards++;
+
+        _cards = new List<Card>();
+
+        СardInitialization();
+        AddCouple();
+    }
+
+    private void СardInitialization()
+    {
+        for (int i = 0; i < _startingCountCards; i++)
+        {
+            Card newCard = Instantiate(_card, transform.position, Quaternion.identity);
+            newCard.transform.SetParent(transform, false);
+            _cards.Add(newCard);
+        }
+    }
+
+    private void AddCouple()
+    {
+        for (int i = 0; i < _cards.Count / 2; i++)
+        {
+            _cards[i].SetNumberCard(i + 1);
+            _cards[_cards.Count / 2 + i].SetNumberCard(i + 1);
+        }
+    }
+
+    private bool EvenCheckNumvers(int numbers)
+    {
+        return numbers % 2 == 0;
+    }
+}
