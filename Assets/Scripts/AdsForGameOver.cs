73b68adf505a41c4ac9deb2296be3bb14b583cd0
Skvorcov Evgeniy <creeger777@gmail.com>
2020-02-12 00:16:58 +0300

    add ads advertisement

---
+++ Assets/Scripts/AdsForGameOver.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class AdsForGameOver : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private GameObject startWatchingButton;
+
+    private string gameId = "3466634";
+    private string myPlacementId = "video";
+    private bool testMode = true;
+
+    private void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+
+        startWatchingButton.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Advertisement.Show(myPlacementId);
+            startWatchingButton.SetActive(false);
+        });
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if(showResult == ShowResult.Finished)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(showResult == ShowResult.Skipped)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(showResult == ShowResult.Failed)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == myPlacementId)
+        {
+            startWatchingButton.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -7,47 +7,10 @@ public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameState _gameState;
     [SerializeField] private TextMeshProUGUI _prizeText;
-    [SerializeField] private TextMeshProUGUI _clockText;
-    [SerializeField] private Button _сontinueGameButton;
-    [SerializeField] private float _timeBetweenSessions;
-
-    private float _sessionCooldown;
 
     private void Awake()
     {
         ShowPrize();
-
-        _sessionCooldown = _timeBetweenSessions;
-        _сontinueGameButton.enabled = false;
-    }
-
-    private void Update()
-    {
-        if (_sessionCooldown > 0)
-            _sessionCooldown -= Time.deltaTime;
-        else
-            _сontinueGameButton.enabled = true;
-
-        ShowСlock();
-    }
-
-    public void OnContinueGameButtonClick()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    private void ShowСlock()
-    {
-        int minutes = (int)_sessionCooldown / 60;
-        int seconds = (int)_sessionCooldown - minutes * 60;
-
-        if (seconds.ToString().Length > 1)
-            _clockText.text = $"{minutes} : {seconds}";
-        else
-            _clockText.text = $"{minutes} : 0{seconds}";
-
-        if (minutes == 0 && seconds == 0)
-            _clockText.text = "Продолжить";
     }
 
     private void ShowPrize()
